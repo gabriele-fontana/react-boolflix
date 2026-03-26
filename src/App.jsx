ae2697abdc1390c53732074fdf6710fa9cc17aeb
gabriele-fontana <gabrielefontana.2001@gmail.com>
2026-03-26 11:21:25 +0100

    show list of movies onSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
-
+import { useState } from "react";
 
 function App() {
   
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=&query='
+  const [title, setTitle] = useState('')
+  const [movies, setMovies] = useState([])
   const api_key = import.meta.env.VITE_API_KEY
-  console.log(api_key);
-  function handleSubmit(){
+  
 
+  function handleSubmit(e){
+
+    e.preventDefault()
+
+    const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${title}`
+
+    fetch(api_url)
+    .then(res => res.json())
+    .then((data)=>{
+      
+      setMovies(data.results)
+      console.log(movies);
+      
+      
+    })
   }
   
   return (
    <>
-    <form>
-      <input type="text" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={title} onChange={(e) => {setTitle(e.target.value)}}/>
       <button>Search</button>
     </form>
+      <div className="movie-list">
+        {movies.length > 0 ? (
+          movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <p><strong>Titolo Originale:</strong> {movie.original_title}</p>
+              <p><strong>Lingua:</strong> {movie.original_language.toUpperCase()}</p>
+              <p><strong>Voto:</strong> {movie.vote_average.toFixed(1)} / 10</p>
+            </div>
+          ))
+        ) : (
+          <p>Nessun film cercato o trovato.</p>
+        )}
+      </div>
+
    </>
   )
 }
