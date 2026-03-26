d52cd603013c94454fbf773e65399b8a258ab06d
gabriele-fontana <gabrielefontana.2001@gmail.com>
2026-03-26 12:41:46 +0100

    add tv series search

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   
   const [title, setTitle] = useState('')
   const [movies, setMovies] = useState([])
+  const [tvSeries, setTvSeries] = useState([])
   const api_key = import.meta.env.VITE_API_KEY
   
 
@@ -12,16 +13,23 @@ function App() {
 
     e.preventDefault()
 
-    const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${title}`
+    const movies_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${title}`
+    const tv_url = `https://api.themoviedb.org/3/search/tv?api_key=${api_key}&query=${title}`
 
-    fetch(api_url)
+    fetch(movies_url)
     .then(res => res.json())
-    .then((data)=>{
+    .then((data) => {
       
       setMovies(data.results)
       console.log(movies);
       
       
+    })
+
+    fetch(tv_url)
+    .then(res => res.json())
+    .then(data => {
+      setTvSeries(data.results)
     })
   }
   
@@ -60,6 +68,36 @@ function App() {
         ))
       ) : (
         <p>Nessun film cercato o trovato.</p>
+      )}
+    </div>
+    <div className="tvSerie-list">
+      {tvSeries.length > 0 ? (
+        tvSeries.map((tvSerie) => (
+          <div key={tvSerie.id}>
+            <h2>{tvSerie.name}</h2>
+            <p><strong>Titolo Originale:</strong> {tvSerie.original_name}</p>
+            <p><strong>Lingua:</strong>
+              <ReactCountryFlag countryCode={
+                tvSerie.original_language === 'en' ? 'GB' :
+                tvSerie.original_language === 'ja' ? 'JP' :
+                tvSerie.original_language === 'he' ? 'IL' :
+                tvSerie.original_language === 'zh' ? 'CN' :
+                tvSerie.original_language.toUpperCase()
+              }
+                svg
+                style={{ marginLeft: '10px', width: '2em', height: '1.5em' }}
+                onError={(e) => {
+                  e.target.style.display = 'none';
+                }} />
+              <span>
+                ({tvSerie.original_language.toUpperCase()})
+              </span>
+            </p>
+            <p><strong>Voto:</strong> {tvSerie.vote_average.toFixed(1)} / 10</p>
+          </div>
+        ))
+      ) : (
+        <p>Nessuna serie cercata o trovata.</p>
       )}
     </div>
 
